96a53f10b6f6f658b7fb634bed8da5193af8988b
PietroAndreoni <pietro.andreoni@outlook.com>
2017-11-27 17:14:23 +0000

    Modificata Sforzo_medio in funzione della  nuova struttura con sforzi 4D. Problema con unità di misura. Mancano modifiche a GIRI SUCCESSIVI

--- Sforzo_medio.m
+++ Sforzo_medio.m
@@ -1,6 +1,6 @@
 function [sm] = Sforzo_medio (riga_cricca,R)
 
-global mesh_iniziale sforzi incidenze
+global mesh_iniziale SF
 
 R = round(R);
 xc= riga_cricca(1);
@@ -14,10 +14,8 @@ for y=yc-R:yc+R
     for z=zc-R:zc+R
         if y>0 && z>0 && y<=dim && z<=dim && mesh_iniziale(xc,y,z)==1    
             k=k+1;
-            i = find(all((incidenze==[xc y z])')); %trova la riga della matrice Sforzi che corrisponde alle coordinate di interesse attraverso una matrice di appoggio che fornisce l'informazione
-            sigma_locale = sforzi(i,:); %seleziona la riga di interesse che contiene i sei elementi del tensore degli sforzi nel punto
-%             Tensore_sigma = [sigma_locale(1) sigma_locale(4) sigma_locale(5); sigma_locale(4) sigma_locale(2) sigma_locale(6); sigma_locale(5) sigma_locale(6) sigma_locale(3)]; %costruisce il tensore degli sforzi
-            Sforzo_massimo(k) = sigma_locale(1) + sigma_locale(4) + sigma_locale(5); %nonzeros(Tensore_sigma*[1 0 0]'); %valuta lo sforzo massimo per la propagazione (perpendicolare alla direzione della cricca)
+            sigma_locale = SF(xc,y,z,:); %seleziona la riga di interesse che contiene i sei elementi del tensore degli sforzi nel punto
+            Sforzo_massimo(k) = sigma_locale(1) + sigma_locale(4) + sigma_locale(5); 
         end
     end
 end
