83bf985f19cd561c21a4997d111c7cf87a23a06a
Simon <zheng815@hotmail.it>
2017-12-03 21:56:56 +0100

    fix bug

--- visualizza_mesh.m
+++ visualizza_mesh.m
@@ -1,15 +1,20 @@
+% prima del run assicurarsi che nella cartella ci siano i file k_2_n_64.mat
+% e k_2_n_200.mat.
+% è consigliato runnare il primo section ogni volta.
 
-global matrice_compressa matrice_modificata
+clear
+clc
+global mesh_modificata
 %% visualizza la mesh 30*30
 load ('k_2_n_64.mat')
 matrice_conversione = matrice_compressa;
-matrice_modificata = matrice_conversione;
+mesh_modificata = matrice_conversione;
 Ricerca_bordi
 for i = 1:30
     for j = 1:30
         for k = 1:30
-            if matrice_modificata(i,j,k) == 0
-                matrice_modificata(i,j,k) = NaN;
+            if mesh_modificata(i,j,k) == 0
+                mesh_modificata(i,j,k) = NaN;
             end
         end
     end
@@ -19,8 +24,8 @@ gridx = (1:30);
 gridy = (1:30);
 gridz = (1:30);
 figure 
-v3d64 = PATCH_3Darray(matrice_modificata,gridx,gridy,gridz);
-matrice_modificata_sezionata(:,1:29,:) = matrice_modificata(:,1:29,:);
+v3d64 = PATCH_3Darray(mesh_modificata,gridx,gridy,gridz);
+matrice_modificata_sezionata(:,1:29,:) = mesh_modificata(:,1:29,:);
 cmap = [1 1 0; 0 0 1];
 figure
 v3d64_sezione = PATCH_3Darray(matrice_modificata_sezionata, gridx, gridy, gridz,'col',cmap);
@@ -51,13 +56,13 @@ v3d64_sezione_cricca = PATCH_3Darray(matrice_modificata_sezionata,gridx,gridy,gr
 load ('k_2_n_200.mat')
 
 matrice_conversione = matrice_compressa;
-matrice_modificata = matrice_conversione;
+mesh_modificata = matrice_conversione;
 Ricerca_bordi
 for i = 1:99
     for j = 1:99
         for k = 1:99
-            if matrice_modificata(i,j,k) == 0
-                matrice_modificata(i,j,k) = NaN(1);
+            if mesh_modificata(i,j,k) == 0
+                mesh_modificata(i,j,k) = NaN(1);
             end
         end
     end
@@ -67,11 +72,9 @@ gridx = (1:99);
 gridy = (1:99);
 gridz = (1:99);
 figure 
-v3d200 = PATCH_3Darray(matrice_modificata,gridx,gridy,gridz);
+v3d200 = PATCH_3Darray(mesh_modificata,gridx,gridy,gridz);
 
-matrice_modificata_sezionata(:,:,1:50) = matrice_modificata(:,:,1:50);
+matrice_modificata_sezionata(:,:,1:50) = mesh_modificata(:,:,1:50);
 cmap = jet(4);
 figure
 v3d200_sezione = PATCH_3Darray(matrice_modificata_sezionata, gridx, gridy, gridz,'col',cmap);
-
-end
