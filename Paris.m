cc44fa3b27f4cf3f2318e17a961ba9c04cbea263
Simon <zheng815@hotmail.it>
2017-12-08 12:52:31 +0100

    Snellimento
    le condizioni del elseif delle righe vecchie 62-68 e 80-86 vengono incorporati nelle if righe 59-69 e 79-90

--- Paris.m
+++ Paris.m
@@ -49,62 +49,66 @@ while N_cicli<10e6
                 matrice_cricche(i,5) = matrice_cricche(i,5) + 10^-3 *C*k^m ; 
                 % incrementa lunghezza cricca
 
-                if  (matrice_cricche(i,5) >= matrice_cricche(i,6)) && ...
-                        N_cicli ~= 1
+                if  (matrice_cricche(i,5) >= matrice_cricche(i,6))
                     % condizione implicita && matrice_cricche(i,7)==1 - se 
                     % la trabecola è già stata modificata deve verificarsi 
                     % la condizone di fallimento.
 
                     matrice_cricche(i,7) = 2;
+                    
+                    if N_cicli ~= 1
+                        % evita, al primo giro, di far uscire la function
+                        % quando alcune cricche hanno lunghezza iniziale 
+                        % che supera lo spessore trabecola. Si preferisce
+                        % vedere la propagazione delle altre cricche.
+                        % nei giri successivi non ha alcun effetto in
+                        % quanto N_cicli viene aggiornato.
                    
-                    flag=1;
-                    
-                elseif (matrice_cricche(i,5) >= matrice_cricche(i,6)) ...
-                        && N_cicli == 1
-                    % condizione per cui la function non esce quando si ha
-                    % una cricca con lunghezza iniziale che supera lo
-                    % spessore della trabecola.
-                    
-                    matrice_cricche(i,7) = 2;
-                
+                        flag=1;
+                        
+                    end
+        
                 elseif matrice_cricche(i,5) >= 0.5*matrice_cricche(i,6) ...
-                        && matrice_cricche(i,7)==0 && N_cicli ~= 1  
+                        && matrice_cricche(i,7)==0  
                     % se la lunghezza della cricca raggiunge la metà dello 
                     % spessore minimo della trabecola e non è già stata 
                     % modificata si rielabora la mesh  
 
                     matrice_cricche(i,7) = 1;
-           
-                    flag=1;
                     
-                elseif matrice_cricche(i,5) >= 0.5*matrice_cricche(i,6) ...
-                        && matrice_cricche(i,7)==0 && N_cicli == 1
-                    % condizione per cui la function non esce quando si ha
-                    % una cricca con lunghezza iniziale che supera lo
-                    % spessore della trabecola.
+                    if N_cicli ~= 1
+                        % evita, al primo giro, di far uscire la function
+                        % quando alcune cricche hanno lunghezza iniziale 
+                        % che supera  la meta' lo spessore trabecola. 
+                        % Si preferisce vedere la propagazione delle altre 
+                        % cricche.
+                        % Nei giri successivi non ha alcun effetto in
+                        % quanto N_cicli viene aggiornato.
+                   
+                        flag=1;
+                        
+                    end
                     
-                    matrice_cricche(i,7) = 1;
+                elseif k > k1c
+                    % se lo sforzo locale supera lo sforzo critico la trabecola
+                    % fallisce immediatamente.
+                    
+                    matrice_cricche(i,7) = 3;
+                    
+                    flag = 1;
                     
                 end
-
-            elseif k > k1c 
-                % se lo sforzo locale supera lo sforzo critico la trabecola
-                % fallisce immediatamente.
-
-                matrice_cricche(i,7) = 3;
-           
-                flag=1;
-                
-            end      
+            end
         end
+        
+        if flag == 1
+            return
+        end
+        
     end
     
-    if flag==1
-        return
-    end
-    
+end
 end
 
-end
 
 
