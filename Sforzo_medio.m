aef682260a0807ed5e8619f325689689903f25d0
Simon <zheng815@hotmail.it>
2017-12-15 16:04:34 +0100

    Fix velocità Paris
    Sforzo_medio.m
    - Riga 11, inizializzazione Sforzo_direzione_carico, lo considero come una variabile double monodimensionale. Quindi si calcola la media a mano con il contatore k. Questo perchè quando avremo molte cricche diventa più veloce.
    - Riga 16, Sforzo_direzione_carico non è più una matrice, viene aggiornato ad ogni giro.
    - Riga 23, Sforzo_direzione_carico non è più una matrice, si calcola la media a mano con il contatore k.
    
    main.m
    - Riga 33, moltiplicato la matrice Sforzi per 13, prima veniva fatto solo sigma_tot nella riga 34. Quindi la matrice sf non veniva creato con i valori giusti. Prima Paris funzionava perchè c'era un *13 che è stato tolto.

--- Sforzo_medio.m
+++ Sforzo_medio.m
@@ -8,18 +8,19 @@ yc= riga_cricca(2);
 zc= riga_cricca(3);
 k=0;
 dim=size(mesh_iniziale,1);
-Sforzo_direzione_carico=0;
+Sforzo_direzione_carico = 0;
 
 for y=yc-R:yc+R
     for z=zc-R:zc+R
         if y>0 && z>0 && y<=dim && z<=dim && mesh_iniziale(xc,y,z)==1    
             k=k+1;
-            sigma_locale = SF(xc,y,z,:); %seleziona la riga di interesse che contiene i sei elementi del tensore degli sforzi nel punto
-            Sforzo_direzione_carico(k) = sigma_locale(2); 
+            Sforzo_direzione_carico = Sforzo_direzione_carico + SF(xc,y,z,2); 
+            %seleziona la riga di interesse che contiene i sei elementi del
+            %tensore degli sforzi nel punto            
         end
     end
 end
 
-sm = abs(mean(Sforzo_direzione_carico));
-
+sm = abs((Sforzo_direzione_carico)/k);
+ 
 end
